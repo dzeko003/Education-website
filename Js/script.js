e9c08657dbe93df816b78e23115645ad24d0abfd
dzeko003 <berenismassamba@gmail.com>
2024-08-16 17:08:23 +0200

    state : courses

--- Js/script.js
+++ Js/script.js
@@ -43,3 +43,57 @@ var swiper = new Swiper(".home-slider", {
     lopp:true,
     grabCusrsor:true,
   });
+
+  var swiper = new Swiper(".home-courses-slider", {
+    lopp:true,
+    grabCusrsor:true,
+    spaceBetween: 20,
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+      },
+    
+  });
+
+  var swiper = new Swiper(".teachers-slider", {
+    lopp:true,
+    grabCusrsor:true,
+    spaceBetween: 20,
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+      },
+    
+  });
+
+  var swiper = new Swiper(".reviews-slider", {
+    lopp:true,
+    grabCusrsor:true,
+    spaceBetween: 20,
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+      },
+    
+  });
